f0952d8c064c60a043d2c6a4f8400c73d4696a28
Adrian Mitev <adrian.mitev@gmail.com>
2025-07-17 09:10:45 +0200

    Add ideas for scales

--- src/app/games/scales/scales.ts
+++ src/app/games/scales/scales.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-scales',
+  selector: 'scales',
   imports: [],
   templateUrl: './scales.html',
   styleUrl: './scales.css'
